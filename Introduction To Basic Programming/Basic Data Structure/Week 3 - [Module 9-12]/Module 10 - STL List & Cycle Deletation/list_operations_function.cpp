d331b784f72016d2e0b78d554a87cee8d9e848e7
erpranto <erpranto55@gmail.com>
2025-08-08 17:44:58 +0600

    some changes

--- Introduction To Basic Programming/Basic Data Structure/Week 3 - [Module 9-12]/Module 10 - STL List & Cycle Deletation/list_operations_function.cpp
+++ Introduction To Basic Programming/Basic Data Structure/Week 3 - [Module 9-12]/Module 10 - STL List & Cycle Deletation/list_operations_function.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
     list<int> l = {10, 20, 30, 60, 20, 40, 80, 100, 10};
-    //l.remove(10); //remove the selected value
-    //l.sort(); //automaticly sort in assending order
-    //l.sort(greater<int>()); //printing desending order
-    
-    //l.unique();
+    // l.remove(10); //remove the selected value
+    // l.sort(); //automaticly sort in assending order
+    // l.sort(greater<int>()); //printing desending order
 
+    l.unique();
+    // To remove duplicate
     l.reverse();
-    
+
     for (int val : l)
     {
         cout << val << " ";
